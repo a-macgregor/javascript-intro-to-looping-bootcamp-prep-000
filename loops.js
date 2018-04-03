a8bf07681c4f0c5e1754ac1d501be7d1e6e9c2c9
A MacGregor <annabel.macgregor@gmail.com>
2018-04-03 20:45:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
-let forLoop() {
+let forLoop = function() {
 
-for (let i = 0; i < 25; i++) {
-  if (i === 1) {
-    forLoop.push("I am 1 strange loop.");
-  } else {
-    forLoop.push(`I am ${i} strange loops.`);
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      forLoop.push("I am 1 strange loop.");
+    } else {
+      forLoop.push(`I am ${i} strange loops.`);
+    }
   }
-}
+};
